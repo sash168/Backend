288a4ada7a727f358a159961dcebd0fdfe89226f
Sashmita <sashmita0209@gmail.com>
2025-04-06 12:34:26 +0530

    changed file

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
+// const gitApi = {
+//     "message": "Not Found",
+//     "documentation_url": "https://docs.github.com/rest",
+//     "status": "404"
+// };
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -11,7 +16,9 @@ app.get('/login', (req, res) => {
     res.send('<h1>Hi please login</h1>')
 })
 
-
+// app.get('/git', (req, res) => {
+//     res.json(gitApi);
+// })
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
